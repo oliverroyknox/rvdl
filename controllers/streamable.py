34c8a0363638025685c58abbae4c5c1c4e43c27e
Oliver Knox <dev@oliverroyknox.com>
2021-09-12 19:10:57 +0100

    added controller to interface with streamable api

---
+++ controllers/streamable.py
@@ -0,0 +1,22 @@
+from exceptions import InvalidQueryException
+
+import re
+import requests
+
+class StreamableController():
+
+    def __init__(self):
+        self.shortcode_rx = re.compile(r"[\/\w*\d*]{6}$")   # assumes shortcode is an alphanumeric string with a length of 6
+
+    def process_clip(self, url):
+        shortcode = re.search(self.shortcode_rx, url).group(0)
+
+        if shortcode is None:
+            raise InvalidQueryException("no valid shortcode detected in supplied url.")
+        
+        response = requests.get(
+            url=f"https://api.streamable.com/videos/{shortcode}",
+            headers={ "User-Agent": "rvdl", "Content-Type": "application/json" }
+        )
+
+        return response.json()
